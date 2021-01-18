df11f7e2a5b09f6a70af6a37cd5260ff098ffb52
aerol <alparslan.erol@boun.edu.tr>
2021-01-18 03:27:54 +0100

    feat(html-css): [PS-21] Create html templates and css static files Template directory has been addded in the repository.

--- hospital/views.py
+++ hospital/views.py
@@ -1,6 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+hospital = [
+    {
+        'name': 'LUXMED',
+        'city': 'Warsaw',
+    },
+    {
+        'name': 'Medical City',
+        'city': 'Wroclaw',
+    }
+]
+
+
 
 def index(request):
     return HttpResponse('<h1>Index</h1>')
+
+
+def home(request):
+    context = {'abc': hospital}
+    return render(request, 'hospital/home.html', context)
+
+
+def about(request):
+    return render(request, 'hospital/about.html', {'title': 'About Title'})
