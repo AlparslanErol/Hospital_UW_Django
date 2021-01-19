ea8604d6f61611ee716415f682a9511f03a163cd
MBurhanKurt <m.burhan.kurt@gmail.com>
2021-01-19 21:57:09 +0300

    feat(django): [PS-22_test_sql] test sql connecion

--- hospital/views.py
+++ hospital/views.py
@@ -1,17 +1,75 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import *
+import sqlite3
 
-hospital = [
-    {
-        'name': 'LUXMED',
-        'city': 'Warsaw',
-    },
-    {
-        'name': 'Medical City',
-        'city': 'Wroclaw',
+
+def dict_factory(cursor, row):
+    d = {}
+    for idx, col in enumerate(cursor.description):
+        d[col[0]] = row[idx]
+    return d
+
+
+def diagnosis(request):
+    con = sqlite3.connect("Hospital.db")
+    con.row_factory = dict_factory
+    cur = con.cursor()
+    cur.execute("""select * from Diagnosis""")
+    temp = cur.fetchall()
+    context = {
+        'abc': temp
     }
-]
+    con.close()
+    return render(request, 'hospital/home.html', context)
 
+
+def doctor(request):
+    con = sqlite3.connect("Hospital.db")
+    con.row_factory = dict_factory
+    cur = con.cursor()
+    cur.execute("""select * from Doctor""")
+    temp = cur.fetchall()
+    context = {
+        'abc': temp
+    }
+    return render(request, 'hospital/home.html', context)
+
+
+def patient(request):
+    con = sqlite3.connect("Hospital.db")
+    con.row_factory = dict_factory
+    cur = con.cursor()
+    cur.execute("""select * from patient""")
+    temp = cur.fetchall()
+    context = {
+        'abc': temp
+    }
+    return render(request, 'hospital/home.html', context)
+
+
+def service(request):
+    con = sqlite3.connect("Hospital.db")
+    con.row_factory = dict_factory
+    cur = con.cursor()
+    cur.execute("""select * from service""")
+    temp = cur.fetchall()
+    context = {
+        'abc': temp
+    }
+    return render(request, 'hospital/home.html', context)
+
+
+def visit(request):
+    con = sqlite3.connect("Hospital.db")
+    con.row_factory = dict_factory
+    cur = con.cursor()
+    cur.execute("""select * from visit""")
+    temp = cur.fetchall()
+    context = {
+        'abc': temp
+    }
+    return render(request, 'hospital/home.html', context)
 
 
 def index(request):
@@ -19,7 +77,9 @@ def index(request):
 
 
 def home(request):
-    context = {'abc': hospital}
+    context = {
+        'abc': abc
+    }
     return render(request, 'hospital/home.html', context)
 
 
